5bb4742561499fd8a3c70483ebeb9422edab2764
LeoLandin <leolandin123@gmail.com>
2021-08-10 23:30:22 -0300

    opinion

--- src/components/Opinion.vue
+++ src/components/Opinion.vue
@@ -4,6 +4,13 @@
         <div>
             <div class="description">
                 <span>testes teste teste testes teste teste testes teste teste testes teste teste testes teste teste testes teste teste testes teste teste testes teste teste testes teste teste testes teste teste testes teste teste testes teste teste testes teste teste testes teste teste testes teste teste testes teste teste testes teste teste testes teste teste testes teste teste testes teste teste testes teste teste  teste teste testes teste teste teste teste testes teste teste teste teste testes teste teste teste teste testes teste teste teste teste testes teste teste teste teste testes teste teste teste teste testes teste teste teste teste testes teste teste teste teste testes teste teste teste teste testes teste teste teste teste testes teste teste teste teste testes teste teste teste teste testes teste teste teste teste testes teste teste teste teste testes teste teste teste teste testes teste teste teste teste testes teste teste teste teste testes teste teste teste teste testes teste teste </span>
+                <div class="userInfo">
+                    <div class="nameOrHour">
+                        <span id="user">Nome do usuário</span>
+                        <span id="hour">10/08/2001 ás 21:14</span>
+                    </div>
+                    <img src="@/assets/user.png">
+                </div>
             </div>
             <div class="images">
                 <img src="" alt="">
@@ -25,6 +32,8 @@ export default {
     flex-direction: column;
     display: flex;
     background-color: white;
+    border-radius: 50px;
+    border: 1px solid white;
 }
 
 .containerOpinion > div {
@@ -32,19 +41,43 @@ export default {
 }
 
 .description {
-    display: flex;
-    flex-direction: row;
-    max-width: 50%;
-    height: 75%;
-    text-align: center;
+    width: 50%;
 }
 
 .description > span {
-    width: 180px;
-    display: inline-block;
-    white-space: pre;
-    text-overflow: ellipsis;
+    display: -webkit-box;
+    max-height: 300px;
+    text-align: justify;
+    -webkit-line-clamp: 10;
+    -webkit-box-orient: vertical;
     overflow: hidden;
-    vertical-align: middle;
+}
+
+.nameOrHour {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+}
+
+#user {
+    font-weight: bold;
+}
+
+#hour {
+    color: #9e9e9e;
+}
+
+.userInfo {
+    display: flex;
+    flex-direction: row;
+    margin-top: 50px;
+    align-items: flex-end;
+    justify-content: flex-end;
+}
+
+img {
+    height: 40px;
+    align-self: center;
+    margin-left: 15px;
 }
 </style>
