a5061d512875414bfe5d735dd51c2411b3358120
LeoLandin <leolandin123@gmail.com>
2021-09-03 20:23:52 -0300

    silencio

---
+++ src/components/backoffice/ListOptions.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="item">
+        <a href="">
+            <img :src="getImage(image)" :alt="name">
+            <span>{{name}}</span>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ListOptions',
+  props: ['image', 'name'],
+  methods: {
+    getImage: (url) => {
+      var images = require.context('../../assets/list/', false, /\.png$/)
+      return images(`./${url}`)
+    }
+  }
+}
+</script>
+
+<style scoped>
+    img {
+        height: 40px;
+    }
+
+    span {
+        margin-left: 10px;
+        font-weight: bold;
+    }
+
+    .item {
+        transition: all 0.4s;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        margin: 10px;
+        cursor: pointer;
+        width: 100%;
+    }
+
+    .item:hover {
+        background-color: #fff7f7;
+        transition: all 0.3s;
+    }
+
+    a {
+        display: flex;
+        flex-direction: row;
+        text-decoration: none;
+        color: black;
+        align-items: center;
+    }
+</style>
--- src/views/backoffice/Admin.vue
+++ src/views/backoffice/Admin.vue
@@ -1,17 +1,27 @@
 <template>
     <div class="background">
-        <div class="containerList">
+        <div class="container">
+            <div class="list">
+                <div v-for="item in list" :key="item.name">
+                    <ListOption :image="item.image" :name="item.name" />
+                </div>
+            </div>
+            <div>
 
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped>
-    .containerList {
+    .container {
         background-color: white;
-        width: 50%;
-        height: 550px;
+        width: 40%;
+        display: flex;
+        height: 460px;
+        flex-direction: row;
         margin-top: 100px;
+        padding: 80px;
     }
 
     .background {
@@ -20,4 +30,44 @@
         align-items: center;
         margin-bottom: 100px;
     }
+
+    .list {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+    }
 </style>
+
+<script>
+import ListOption from '@/components/backoffice/ListOptions.vue'
+
+export default {
+  components: { ListOption },
+  data () {
+    return {
+      list: [
+        {
+          name: 'Hospedagem',
+          image: 'icon-hotel.png'
+        },
+        {
+          name: 'Passeio Quadriciclo',
+          image: 'icon-quadriciclo.png'
+        },
+        {
+          name: 'Passeio Banana Boat',
+          image: 'icon-banana.png'
+        },
+        {
+          name: 'Passeio 4X4',
+          image: 'icon-4x4.png'
+        },
+        {
+          name: 'Capitólio e Serra',
+          image: 'icon-montanha.png'
+        }
+      ]
+    }
+  }
+}
+</script>
