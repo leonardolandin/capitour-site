f0365c22d0b8af4bbb9a3037850149653d682ff5
LeoLandin <leolandin123@gmail.com>
2021-09-07 14:28:51 -0300

    silencio

---
+++ src/components/backoffice/Card.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="containerCard">
+        <div class="info">
+            <span id="title">Titulo blablablablablabla blablablablablabla blablablablablabla blablablablablabla</span>
+            <span id="date">Data e hora</span>
+        </div>
+        <div class="actions">
+            <div title="Editar">
+                <img src="@/assets/card/edit.png">
+            </div>
+            <div title="Remover">
+                <img src="@/assets/card/remove.png">
+            </div>
+            <div title="Ativar">
+                <img src="@/assets/card/accept.png">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Card'
+}
+</script>
+
+<style scoped>
+    .containerCard {
+        display: flex;
+        flex-direction: row;
+        border: 2px solid #eceaea;
+        border-radius: 7px;
+        padding: 10px;
+        justify-content: space-between;
+    }
+
+    .info {
+        display: flex;
+        flex-direction: column;
+    }
+
+    #title {
+        display: -webkit-box;
+        -webkit-line-clamp: 1;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        width: 300px;
+        font-weight: bold;
+        margin-bottom: 10px;
+    }
+
+    #date {
+        color: #9e9e9e;
+    }
+
+    .actions {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    img {
+        height: 35px;
+        cursor: pointer;
+    }
+</style>
--- src/views/backoffice/Admin.vue
+++ src/views/backoffice/Admin.vue
@@ -1,48 +1,27 @@
 <template>
     <div class="background">
         <div class="container">
-            <div class="list">
-                <div v-for="item in list" :key="item.name">
-                    <ListOption :image="item.image" :name="item.name" />
-                </div>
+            <div class="listOption">
+              <div v-for="item in list" :key="item.name">
+                <ListOption :image="item.image" :name="item.name" />
+              </div>
             </div>
-            <div>
-
+            <div class="containerClear"><div class="clearLine"/></div>
+            <div class="sizeCards">
+              <div>
+                <Card />
+              </div>
             </div>
         </div>
     </div>
 </template>
 
-<style scoped>
-    .container {
-        background-color: white;
-        width: 40%;
-        display: flex;
-        height: 460px;
-        flex-direction: row;
-        margin-top: 100px;
-        padding: 80px;
-    }
-
-    .background {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-bottom: 100px;
-    }
-
-    .list {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-    }
-</style>
-
 <script>
 import ListOption from '@/components/backoffice/ListOptions.vue'
+import Card from '@/components/backoffice/Card.vue'
 
 export default {
-  components: { ListOption },
+  components: { ListOption, Card },
   data () {
     return {
       list: [
@@ -71,3 +50,46 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+    .container {
+      background-color: white;
+      width: 40%;
+      display: flex;
+      height: 460px;
+      flex-direction: row;
+      margin-top: 100px;
+      padding: 80px;
+      justify-content: space-between;
+    }
+
+    .background {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-bottom: 100px;
+    }
+
+    .listOption {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+    }
+
+    .clearLine {
+      border: 1px solid #e4e4e4;
+      border-block-end: none;
+      width: 10px;
+      border-top: none;
+      border-bottom: none;
+    }
+
+    .containerClear {
+      display: flex;
+      justify-content: center;
+    }
+
+    .sizeCards {
+      width: 60%;
+    }
+</style>
